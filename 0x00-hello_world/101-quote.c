89d55191fbb7fd65066653c865424ea4d798f095
muturrii <karugu.muturi@gmail.com>
2023-02-16 15:38:07 -0800

    last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,5 @@ int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
