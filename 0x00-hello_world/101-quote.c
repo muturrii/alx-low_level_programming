7b1d995cde2bdaa00fdcae7bdcce740e1302045c
muturrii <karugu.muturi@gmail.com>
2023-02-16 15:30:42 -0800

    last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
  */
 int write(int filedes, const char *buf, unsigned int nbyte);
-int main(int argc, char **argv)
+int main()
 {
 	write(1, "Hello World!\n", 13);
 	return (0);
