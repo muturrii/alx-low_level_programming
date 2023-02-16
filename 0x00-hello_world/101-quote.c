d1a695d4d15be77e5100db722f36b0a03c6bee42
muturrii <karugu.muturi@gmail.com>
2023-02-16 15:28:44 -0800

    last fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <unistd.h>
-#include <sys/syscall.h>
+#include <stdio.h>
 /**
  * main - prints the string
  * main is Entry point
- *
+ * @filedes: return value
+ * @buf: string
+ * @nbyte: length
  * Return: Always 0 (Success)
  */
-int main(void)
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main(int argc, char **argv)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	syscall(SYS_write, 1, msg);
+	write(1, "Hello World!\n", 13);
 	return (0);
 }
