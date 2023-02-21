0285e795dec3a8652552268ab576cb3cd504bfdf
muturrii <karugu.muturi@gmail.com>
2023-02-21 12:30:53 -0800

    nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,18 +10,29 @@ void times_table(void)
 	int j;
 	int k;
 	int i;
+	int a;
+	int b;
 	int ans;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(0);
 		_putchar(',');
 		for (j = 0; j < 2; j++)
 			_putchar(' ');
 		for (k = 0; k < 10; k++)
 		{
 			ans = i * k;
-			_putchar(ans);
+			if (ans > 9)
+			{
+				a = ans % 10;
+				b = ans / 10;
+				_putchar(b);
+				_putchar(a);
+			}
+			else
+			{
+				_putchar(ans);
+			}
 		}
 		_putchar('\n');
 	}
