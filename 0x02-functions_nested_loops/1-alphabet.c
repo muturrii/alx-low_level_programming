928a975f919c26e10a9d7a4b3fa3821550450527
muturrii <karugu.muturi@gmail.com>
2023-02-21 08:02:43 -0800

    number two

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet a to z
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+		_putchar('a' + i);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_alphabet(void);
 int _putchar(int);
+void print_alphabet(void);
