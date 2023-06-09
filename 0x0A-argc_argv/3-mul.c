3f5a1b819f34d72443ae41c5b96407caaa312ab6
Muturrii <122809798+muturrii@users.noreply.github.com>
2023-06-09 21:08:35 +0300

    Add files via upload

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints product of two numbers
+ * @argc: contains number of arguments
+ * @argv: array of strings passed
+ * Return: return (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Learning about main parameters argc and argv[]
